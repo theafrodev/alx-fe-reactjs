9630aaacbdfdbea8ecbe1ba6222817e4b9ec588d
theafrodev <fialorchristian@gmail.com>
2024-08-11 20:38:14 +0000

    create specific components in react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -9,7 +12,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +31,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </>
   )
 }
