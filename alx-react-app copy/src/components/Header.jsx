b77960e0c7712f26a7ce20af43f176cea8b968e9
theafrodev <fialorchristian@gmail.com>
2024-08-24 23:05:42 +0000

    reconciliation

--- alx-react-app copy/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header(){
-    return(
-        <header>
-            <h1>My Favorite Cities</h1>
-        </header>
-    );
-}
--- alx-react-app copy/src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-export default function MainContent(){
-    return(
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
-}
