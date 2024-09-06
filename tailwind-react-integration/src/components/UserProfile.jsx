82d3c6f5484b23266ef7103e0a6eb29134d4ce6e
theafrodev <fialorchristian@gmail.com>
2024-09-06 16:05:07 +0000

    responsive sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-        <img className="rounded-full w-36 h-36 mx-auto bg-blue-800" src="https://via.placeholder.com/150" alt="User" />
-        <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <img className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto bg-blue-200" src="https://via.placeholder.com/150" alt="User" />
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
