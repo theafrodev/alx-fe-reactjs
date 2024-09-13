8bf2980ee09cc0f929ef879824d7742d33d2ee63
theafrodev <fialorchristian@gmail.com>
2024-09-13 15:10:11 +0000

    added sm

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -30,7 +30,7 @@ function HomePage() {
   }
 
   return (
-    <div className="container grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto">
+    <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto">
       {recipes && recipeArray}
     </div>
   )
