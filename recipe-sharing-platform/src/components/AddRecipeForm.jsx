dbeda7502eaf5ec7507cbb387944796a37b1c26f
theafrodev <fialorchristian@gmail.com>
2024-09-15 21:38:53 +0000

    form validation

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -8,9 +8,57 @@ function AddRecipeForm() {
     const [ingredients, setIngredients] = useState('');
     const [steps, setSteps] = useState('');
 
+    const [errors, setErrors] = useState({});
+
+    const validateForm = () => {
+        const newErrors = {};
+        
+        if (!title || title.trim() === '') {
+            newErrors.title = 'Recipe name is required.';
+        }
+
+        if (!description || description.trim() === '') {
+            newErrors.description = 'Description is required.';
+        }
+
+        if (!ingredients || ingredients.trim() === '') {
+            newErrors.ingredients = 'Ingredients are required.';
+        }
+
+        if (!steps || steps.trim() === '') {
+            newErrors.steps = 'Instructions are required.';
+        }
+
+        return newErrors;
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        const recipe = {title, summary, image, ingredients, instructions}
+
+        const newErrors = validateForm();
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+          } else {
+
+            setErrors({});
+
+        const recipe = {
+            title,
+            summary: description,
+            image: 'https://via.placeholder.com/150',
+            ingredients: ingredients.split('\n'),
+            instructions: steps.split('\n'),
+        }
+
+        console.log(recipe);
+
+        setTitle('');
+        setDescription('');
+        setIngredients('');
+        setSteps('');
+        
+        }
     }
 
   return (
@@ -24,6 +72,7 @@ function AddRecipeForm() {
             value={title} 
             required 
             onChange={(e)=>setTitle(e.target.value)}/>
+            {errors.title && <h6>{errors.title}</h6>}
 
         <label htmlFor="">Description</label>
         <textarea 
@@ -33,6 +82,7 @@ function AddRecipeForm() {
             required 
             onChange={(e)=>setDescription(e.target.value)} 
             ></textarea>
+            {errors.description && <h6>{errors.description}</h6>}
 
         <label htmlFor="">Ingredients</label>
         <textarea 
@@ -41,6 +91,7 @@ function AddRecipeForm() {
             value={ingredients} 
             required 
             onChange={(e)=>setIngredients(e.target.value)} ></textarea>
+            {errors.ingredients && <h6>{errors.ingredients}</h6>}
 
         <label htmlFor="">Instructions</label>
         <textarea 
@@ -49,6 +100,7 @@ function AddRecipeForm() {
             value={steps} 
             required 
             onChange={(e)=>setSteps(e.target.value)} ></textarea>
+            {errors.instructions && <h6>{errors.instructions}</h6>}
 
         <button>Add Recipe</button>
       </form>
