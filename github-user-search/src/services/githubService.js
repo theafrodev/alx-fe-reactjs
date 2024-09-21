3f18cf705b0c2d3b4c09247ddc8676dcd09e6624
theafrodev <fialorchristian@gmail.com>
2024-09-21 09:04:23 +0000

    switch to axios

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,36 @@
+// const GITHUB_API_KEY = import.meta.env.VITE_GITHUB_API_KEY;
+
+// export const fetchUserData = async (username) => {
+//   const response = await fetch(`https://api.github.com/users/${username}`, {
+//     headers: {
+//       Authorization: `Bearer ${GITHUB_API_KEY}`,
+//     },
+//   });
+
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch GitHub data");
+//   }
+
+//   const data = await response.json();
+//   return data;
+// };
+
+
+import axios from 'axios';
+
 const GITHUB_API_KEY = import.meta.env.VITE_GITHUB_API_KEY;
 
 export const fetchUserData = async (username) => {
-  const response = await fetch(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: `Bearer ${GITHUB_API_KEY}`,
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch GitHub data");
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_API_KEY}`,
+      },
+    });
+    
+    return response.data;
+    
+  } catch (error) {
+    throw new Error(`Failed to fetch GitHub data: ${error.response?.status || error.message}`);
   }
-
-  const data = await response.json();
-  return data;
 };
